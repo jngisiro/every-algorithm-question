810ed827c6e38e13118bc0384e07ca33956edded
Nico <nico@skubos.com>
2019-10-15 11:51:33 +0700

    adds capitalize and duplicates

---
+++ Algos/areThereDuplicates.js
@@ -0,0 +1,37 @@
+// Write a function called areThereDuplicates
+// which accepts a variable number of arguements
+// and checks whether there are any duplicates among the arguments passed in
+// you can solve this using the frequency counter pattern or the multiple pointers pattern
+// example:
+// function(1, 2, 3) // false
+// function(1, 2, 2) // true
+// function('a', 'b', 'c', 'd') // true
+
+
+// Frequency Counter Solution 
+function areThereDuplicates() {
+  let collection = {}
+  for (let val in arguments) {
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+  }
+  for (let key in collection) {
+    if (collection[key] > 1) return true
+  }
+  return false;
+}
+
+// Multiple Pointers Solution 
+function areThereDuplicates(...args) {
+  // Two pointers
+  args.sort((a, b) => a > b);
+  let start = 0;
+  let next = 1;
+  while (next < args.length) {
+    if (args[start] === args[next]) {
+      return true
+    }
+    start++
+    next++
+  }
+  return false
+}
---
+++ Algos/sameFrequency.js
@@ -0,0 +1,31 @@
+// Write a function called sameFrequency
+// Given two positive integers
+// Find out if the two numbers have the same frequency of digits
+// Sample Input: 
+// sameFrequency(182,281) // true
+// sameFrequency(24, 14) // false
+// sameFrequency(22, 222) // false
+
+
+function sameFrequency(num1, num2) {
+  let strNum1 = num1.toString();
+  let strNum2 = num2.toString();
+  if (strNum1.length !== strNum2.length) return false;
+
+  let countNum1 = {};
+  let countNum2 = {};
+
+  for (let i = 0; i < strNum1.length; i++) {
+    countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1
+  }
+
+  for (let j = 0; j < strNum1.length; j++) {
+    countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1
+  }
+
+  for (let key in countNum1) {
+    if (countNum1[key] !== countNum2[key]) return false;
+  }
+
+  return true;
+}
